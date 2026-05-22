d1e71cb1b5bac961e6bbe64a4a408139ebb1598f
SadSatan <santiagovalentixd@gmail.com>
2026-05-22 15:57:57 -0300

    First commit

---
+++ src/test/java/com/analistas/compraventanea/CompraventaneaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.analistas.compraventanea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompraventaneaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
